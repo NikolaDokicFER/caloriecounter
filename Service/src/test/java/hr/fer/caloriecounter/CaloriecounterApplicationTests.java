8351930410f6b15bd5032a9aea9000a0b8483400
unknown <nd51917@fer.hr>
2022-03-14 19:49:14 +0100

    Initial commit

---
+++ Service/src/test/java/hr/fer/caloriecounter/CaloriecounterApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.caloriecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriecounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
